e996b3a618bbd112f43f59f046ee98b9799e9a54
Tehnikr <bswcct@gmail.com>
2022-10-14 11:26:15 +0300

    1

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.catsgram.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private String email;
+    private String nickname;
+    private LocalDate birthdate;
+}
